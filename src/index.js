9d80cb7deab8d28a3f3ddccc2b55e15f4dd655ba
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-15 17:19:56 +0200

    setting up sqlite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const PORT = 3030;
+
+let db = new sqlite3.Database('src/db/project.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to DB.');
+});
+
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
 
 app.listen(PORT, () => console.log(`Server is listenting on port ${PORT}...`));
