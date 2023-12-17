4cbb37b0c5d0fec86596ae6c35527ca661f73e79
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-17 17:52:30 +0200

    added express configuration

--- src/index.js
+++ src/index.js
@@ -6,5 +6,8 @@ const db = require('./config/db');
 const app = express();
 const PORT = 3030;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 app.listen(PORT, () => console.log(`Server is listenting on port ${PORT}...`));
