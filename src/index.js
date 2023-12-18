00af9a2f84fcd501b12443cc1fe05e3ca701ccc6
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-18 15:12:35 +0200

    added cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 
 // Connecting to DB
 const db = require('./config/db');
@@ -8,6 +9,6 @@ const PORT = 3030;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Server is listenting on port ${PORT}...`));
