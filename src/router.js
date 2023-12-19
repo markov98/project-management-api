a9877b01f9484f423d7319f4d723a20e2abc7121
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-19 18:17:14 +0200

    added default route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const userController = require('./controllers/userController');
 
+router.get('/', (req, res) => {
+    res.send('Hello! This is Project Management API!')
+})
+
 router.use('/users', userController);
 
 module.exports = router;
