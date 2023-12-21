0fb2be8994781c3fef46b2ddf97ad5372214f74a
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-21 20:52:44 +0200

    added register path with functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const userService = require('../services/userService');
 
-router.post('/login', (req, res) => {
-    res.json('Testing')
+router.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        await userService.register(username, email, password);
+        res.send('Registration Successful!')
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 })
 
 module.exports = router;
