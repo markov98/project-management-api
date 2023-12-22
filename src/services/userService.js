5a11aa0e59c497e2c3042eddce932f66761be0c6
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-22 18:44:05 +0200

    made register function a promise

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,8 +6,16 @@ const db = require('../config/db');
 exports.register = async (username, email, password) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    db.run(`
-    INSERT INTO users (username, email, password)
-    VALUES (?, ?, ?)
-`, [username, email, hashedPassword]);
+    return new Promise((resolve, reject) => {
+        db.run(`
+            INSERT INTO users (username, email, password)
+            VALUES (?, ?, ?)
+        `, [username, email, hashedPassword], function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve('Succes!');
+            }
+        });
+    });
 };
