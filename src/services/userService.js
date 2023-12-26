eec9231da7809a66fb04ee6a0b5aa86a93d4fb56
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-26 11:59:55 +0200

    updated user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,56 +1,49 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const util = require('util');
 
 const db = require('../config/db');
+
+const dbRunAsync = util.promisify(db.run).bind(db);
+const dbGetAsync = util.promisify(db.get).bind(db);
 
 exports.register = async (username, email, password) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new Promise((resolve, reject) => {
-        db.run(`
+    await dbRunAsync(`
             INSERT INTO users (username, email, password)
             VALUES (?, ?, ?)
-        `, [username, email, hashedPassword], function (err) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({id: this.lastID, email});
-            }
-        });
-    });
+        `, [username, email, hashedPassword]);
 
-    return getResult(await user);
+    const user = { id: this.lastID, email };
+    return getResult(user);
+
 };
 
 exports.login = async (email, password) => {
-    const user = await new Promise((resolve, reject) => {
-        db.get('SELECT id, email, username, password FROM users WHERE email = ?', [email], (err, row) => {
-            if (err) reject(err);
-            resolve(row);
-        });
-    });
+    const user = await dbGetAsync('SELECT id, email, username, password FROM users WHERE email = ?', [email]);
 
-if (!user) {
-    throw new Error('User not found');
-}
+    if (!user) {
+        throw new Error('User not found');
+    }
 
-const passwordMatch = await bcrypt.compare(password, user.password);
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
-if (!passwordMatch) {
-    throw new Error('Incorrect password');
-}
+    if (!passwordMatch) {
+        throw new Error('Incorrect password');
+    }
 
-return getResult(user);
-}
+    return getResult(user);
+};
 
 function getResult(user) {
     const payload = { _id: user._id, email: user.email };
-    const token = jwt.sign(payload, "SOME_SECRET", { expiresIn: "1d" });  
+    const token = jwt.sign(payload, "SOME_SECRET", { expiresIn: "1d" });
     const result = {
-      _id: user._id,
-      accessToken: token,
-      email: user.email,
+        _id: user._id,
+        accessToken: token,
+        email: user.email,
     };
-  
+
     return result;
-  }
+}
