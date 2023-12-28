d7060d35ad47ec3426354221c2cc030458a68226
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-28 19:05:01 +0200

    added role adding functionality

--- src/controllers/rolesController.js
+++ src/controllers/rolesController.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const rolesService = require('../services/rolesService');
 
 router.post('/add', async (req, res) => {
-    res.send('Test')
+    try {
+        const { roleName, description } = req.body
+        await rolesService.add(roleName, description);
+        res.send('Role added');
+    } catch (err) {
+        res.status(404).json(err.message)
+    }
 })
 
 module.exports = router;
--- src/services/rolesService.js
+++ src/services/rolesService.js
@@ -1,3 +1,8 @@
-exports.add = async () => {
-    
+const db = require("../config/db");
+
+exports.add = async (roleName, description) => {
+    await db.asyncRun(`
+    INSERT INTO roles (role_name, description)
+    VALUES (?, ?)
+`, [roleName, description]);
 }
